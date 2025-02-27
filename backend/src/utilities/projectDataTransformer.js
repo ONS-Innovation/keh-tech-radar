f9d008ba02bfdd005d2340f3e4635d75b7f4bb6f
Sebastian <Sebastian.Evison-Theodossiades@ons.gov.uk>
2025-02-27 13:20:41 +0000

    get project description, programme/short name

--- backend/src/utilities/projectDataTransformer.js
+++ backend/src/utilities/projectDataTransformer.js
@@ -19,6 +19,9 @@ function transformProjectToCSVFormat(project) {
   return {
     Project: project.details[0]?.name || "",
     Project_Short: project.details[0]?.short_name || "",
+    Programme: project.details[0]?.programme || "",
+    Programme_Short: project.details[0]?.programme_short || "",
+    Description: project.details[0]?.description || "",
     Project_Area: "",
     DST_Area: "",
     Team: technicalContact,
